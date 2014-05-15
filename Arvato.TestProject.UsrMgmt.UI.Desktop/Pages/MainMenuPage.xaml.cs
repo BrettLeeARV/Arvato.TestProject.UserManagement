d90fa71fc37200c142bed141051bd53aa6667119
Tan Jing Tao <jingtao.tan@bertelsmann.de>
2014-05-15 10:27:32 +0800

    Hooked up delete user logic to desktop UI

--- Arvato.TestProject.UsrMgmt.UI.Desktop/Pages/MainMenuPage.xaml.cs
+++ Arvato.TestProject.UsrMgmt.UI.Desktop/Pages/MainMenuPage.xaml.cs
@@ -13,6 +13,8 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Arvato.TestProject.UsrMgmt.UI.Desktop.ViewModels;
 using Arvato.TestProject.UsrMgmt.Entity.Model;
+using Arvato.TestProject.UsrMgmt.BLL.Interface;
+using Arvato.TestProject.UsrMgmt.BLL.Service;
 
 namespace Arvato.TestProject.UsrMgmt.UI.Desktop
 {
@@ -82,7 +84,27 @@ namespace Arvato.TestProject.UsrMgmt.UI.Desktop
 
         private void deleteUserButton_Click(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("Delete User screen coming soon... for user ID " + selectedUser.ID);
+            var message = String.Format("Are you sure you want to delete the user \"{0}\"?", selectedUser.LoginID);
+            var result = MessageBox.Show(message, "Deleting user", MessageBoxButton.YesNo, MessageBoxImage.Warning);
+            
+            if (result == MessageBoxResult.Yes)
+            {
+                var success = false;
+                try
+                {
+                    IUserService userService = new UserService();
+                    userService.Delete(selectedUser);
+                    success = true;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Couldn't delete user", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+                if (success)
+                {
+                    NavigationService.Refresh();
+                }
+            }
         }
     }
 }
