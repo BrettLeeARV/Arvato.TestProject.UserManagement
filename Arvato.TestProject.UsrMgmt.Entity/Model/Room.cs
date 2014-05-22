61a654227cec63de04ad9fcf2ed41c6b34bce818
Benjaminshafa <Benjamin.sh@hotmail.com>
2014-05-22 15:33:35 +0800

    Bookin

---
+++ Arvato.TestProject.UsrMgmt.Entity/Model/Room.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Arvato.TestProject.UsrMgmt.Entity.Model
+{
+    public class Room
+    {
+        public virtual int ID { get; set; }
+        public virtual string Name { get; set; }
+        public virtual string Location { get; set; }
+        public virtual int Capacity { get; set; }
+        public virtual bool IsEnabled { get; set; }
+    }
+}
