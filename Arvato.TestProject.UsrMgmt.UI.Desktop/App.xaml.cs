0b68381e7279ec4eca8fc6a44d136c3517e45ed6
Tan Jing Tao <jingtao.tan@bertelsmann.de>
2014-05-23 16:42:38 +0800

    Added global app unhandled exception handler

--- Arvato.TestProject.UsrMgmt.UI.Desktop/App.xaml.cs
+++ Arvato.TestProject.UsrMgmt.UI.Desktop/App.xaml.cs
@@ -12,5 +12,28 @@ namespace Arvato.TestProject.UsrMgmt.UI.Desktop
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            AppDomain.CurrentDomain.UnhandledException += new
+               UnhandledExceptionEventHandler(this.AppDomainUnhandledExceptionHandler);
+
+            base.OnStartup(e);
+        }
+        
+        void AppDomainUnhandledExceptionHandler(object sender, UnhandledExceptionEventArgs ea)
+        {
+            MessageBox.Show(
+                String.Format(@"An unhandled exception has occured:
+{0}
+
+The application will now close.", ((Exception)ea.ExceptionObject).Message),
+                "Fatal error",
+                MessageBoxButton.OK,
+                MessageBoxImage.Error);
+
+            // Exit app because when the exception has reached the "point of no return"
+            Application.Current.Shutdown();
+        }
+
     }
 }
