7625f2fbf66d975840400ca9794e007673547e02
Benjaminshafa <Benjamin.sh@hotmail.com>
2014-05-26 17:48:57 +0800

    Select Asset based on ID.
    A new method has been added to the solution to retrieve the asset
    details based on Asset ID.

--- Arvato.TestProject.UsrMgmt.DAL/Repository/AssetRepository.cs
+++ Arvato.TestProject.UsrMgmt.DAL/Repository/AssetRepository.cs
@@ -8,64 +8,95 @@ using Arvato.TestProject.UsrMgmt.DAL.Interface;
 
 namespace Arvato.TestProject.UsrMgmt.DAL.Repository
 {
-  public class AssetRepository : BaseRepository, IAssetRepository
+    public class AssetRepository : BaseRepository, IAssetRepository
     {
-              public AssetRepository(SqlConnection dbConnection):base(dbConnection)
+        public AssetRepository(SqlConnection dbConnection)
+            : base(dbConnection)
         {
-            
+
         }
-      public IQueryable<Asset> GetList()
-      {
-          try
-          {
-              SessionFactory sf = new SessionFactory();
-              var factory = sf.CreateSessionFactory();
-              using (var session = factory.OpenSession())
-              {
-                  var Fields = session.CreateQuery("FROM Asset").List<Asset>();
-                  return Fields.AsQueryable<Asset>();
-              }
-          }
-          catch (Exception ex)
-          {
-              
-              throw ex;
-          }
-      }
-      public bool InsertAsset(Asset entitiy)
-      {
-          bool result = false;
+        public IQueryable<Asset> GetList()
+        {
+            try
+            {
+                SessionFactory sf = new SessionFactory();
+                var factory = sf.CreateSessionFactory();
+                using (var session = factory.OpenSession())
+                {
+                    var Fields = session.CreateQuery("FROM Asset").List<Asset>();
+                    return Fields.AsQueryable<Asset>();
+                }
+            }
+            catch (Exception ex)
+            {
 
-          try
-          {
-              SessionFactory sf = new SessionFactory();
-              var factory = sf.CreateSessionFactory();
+                throw ex;
+            }
+        }
+        public bool InsertAsset(Asset entitiy)
+        {
+            bool result = false;
 
-              using (var session = factory.OpenSession())
-              {
+            try
+            {
+                SessionFactory sf = new SessionFactory();
+                var factory = sf.CreateSessionFactory();
 
+                using (var session = factory.OpenSession())
+                {     
+                    session.Save(entitiy);
+                    return true;
+                }
+            }
+            catch (Exception)
+            {
+                return false;
+                throw;
+            }
+        }
+        public bool UpdateAsset(Asset entity)
+        {
+            try
+            {
+                SessionFactory sf = new SessionFactory();
+                var factory = sf.CreateSessionFactory();
 
+                using (var session = factory.OpenSession())
+                {
 
-                  var asset = new Asset
-                  {
+          
+                    session.Update(entity);
 
-                      RoomID = entitiy.RoomID,
-                      Name = entitiy.Name,
-                      IsEnabled = entitiy.IsEnabled
-                  };
-                  session.Save(asset);
-                  
-                  return true;
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
 
+                throw ex;
+            }
+        }
 
-              }
+        public Asset SelectAsset(Asset entity)
+        {
+            try
+            {
+                SessionFactory sf = new SessionFactory();
+                var factory = sf.CreateSessionFactory();
 
-          }
-          catch (Exception)
-          {
-              return false;
-              throw;
-          }
-      }
+                using (var session = factory.OpenSession())
+                {
+
+                    var asset = session.QueryOver<Asset>().Where(x => x.ID == entity.ID).SingleOrDefault(); 
+
+                    return asset;
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
     }
 }
---
+++ Arvato.TestProject.UsrMgmt.UI.Web/UpdateAsset.aspx
@@ -0,0 +1,39 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="UpdateAsset.aspx.cs" Inherits="Arvato.TestProject.UsrMgmt.Web.UI.UpdateAsset" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:Label ID="Label1" runat="server" Text="AssetID:"></asp:Label>
+&nbsp;<asp:TextBox ID="txtassetID" runat="server"></asp:TextBox>
+        &nbsp;
+        <asp:Button ID="Button3" runat="server" Text="ViewAsset" 
+            onclick="Button3_Click" />
+        <br />
+        <br />
+        RoomID:
+        <asp:TextBox ID="txtroomid" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        Name:<asp:TextBox ID="txtname" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        <asp:CheckBox ID="CheckBox1" runat="server" Text="IsEnabled" />
+    
+        <br />
+        <br />
+        <asp:Button ID="Button1" runat="server" Text="Update" 
+           />
+&nbsp;
+        <asp:Button ID="Button2" runat="server" Text="Delete" />
+    
+    </div>
+    </form>
+</body>
+</html>
