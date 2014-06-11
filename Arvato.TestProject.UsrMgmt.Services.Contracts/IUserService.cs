e5cf37b2a3bda802ecc8713ed4d179fe26f4e8e5
Tan Jing Tao <jingtao.tan@bertelsmann.de>
2014-06-11 09:45:57 +0800

    Implemented User Service

---
+++ Arvato.TestProject.UsrMgmt.Services.Contracts/IUserService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using Arvato.TestProject.UsrMgmt.Services.Contracts.DataContracts;
+using Arvato.TestProject.UsrMgmt.Entity.Model;
+
+namespace Arvato.TestProject.UsrMgmt.Services.Contracts
+{
+    [ServiceContract]
+    public interface IUserService
+    {
+        [OperationContract]
+        IEnumerable<User> GetList();
+
+        [OperationContract]
+        void Save(User user);
+
+        [OperationContract]
+        void Delete(User user);
+
+        [OperationContract]
+        bool Login(User user);
+
+        [OperationContract]
+        bool IsExistingLoginID(string loginID, int userID);
+    }
+}
---
+++ Arvato.TestProject.UsrMgmt.Services/UserService.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using Arvato.TestProject.UsrMgmt.BLL.Interface;
+using Arvato.TestProject.UsrMgmt.Services.Contracts;
+using Arvato.TestProject.UsrMgmt.Services.Contracts.DataContracts;
+using Arvato.TestProject.UsrMgmt.BLL.Component;
+using Arvato.TestProject.UsrMgmt.Entity.Model;
+
+namespace Arvato.TestProject.UsrMgmt.Services
+{
+    public class UserService : Contracts.IUserService
+    {
+
+        public IEnumerable<User> GetList()
+        {
+            IUserComponent component = new UserComponent();
+            var results = component.GetList();
+            return results;
+        }
+
+        public void Save(User user)
+        {
+            IUserComponent component = new UserComponent();
+            try
+            {
+                component.Save(user);
+            }
+            catch (Exception e)
+            {
+                throw new FaultException(e.Message);
+            }
+        }
+
+        public void Delete(User user)
+        {
+            IUserComponent component = new UserComponent();
+            try
+            {
+                component.Delete(user);
+            }
+            catch (Exception e)
+            {
+                throw new FaultException(e.Message);
+            }
+        }
+
+        public bool Login(User user)
+        {
+            IUserComponent component = new UserComponent();
+            bool result = false;
+            try
+            {
+                result = component.Login(user);
+            }
+            catch (Exception e)
+            {
+                throw new FaultException(e.Message);
+            }
+            return result;
+        }
+
+        public bool IsExistingLoginID(string loginID, int userID)
+        {
+            IUserComponent component = new UserComponent();
+            bool result = false;
+            try
+            {
+                result = component.IsExistingLoginID(loginID, userID);
+            }
+            catch (Exception e)
+            {
+                throw new FaultException(e.Message);
+            }
+            return result;
+        }
+    }
+}
