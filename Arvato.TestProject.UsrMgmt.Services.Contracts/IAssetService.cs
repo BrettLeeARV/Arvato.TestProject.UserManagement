9e634712077b409a9b3ca10820f01a9106da2ac4
Benjaminshafa <Benjamin.sh@hotmail.com>
2014-06-11 16:14:34 +0800

    Asset
    Asset Service has been implemented.

---
+++ Arvato.TestProject.UsrMgmt.Services.Contracts/IAssetService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using Arvato.TestProject.UsrMgmt.Entity.Model;
+
+namespace Arvato.TestProject.UsrMgmt.Services.Contracts
+{
+    [ServiceContract]
+    public interface IAssetService
+    {
+        [OperationContract]
+        IEnumerable<Asset> GetList(bool Enabled);
+
+        [OperationContract]
+        bool InsertAsset(Asset asset);
+
+        [OperationContract]
+        bool UpdateAsset(Asset entity);
+
+       
+
+    }
+}
---
+++ Arvato.TestProject.UsrMgmt.Services/AssetService.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+using Arvato.TestProject.UsrMgmt.BLL.Interface;
+using Arvato.TestProject.UsrMgmt.BLL.Component;
+using System.Collections;
+using Arvato.TestProject.UsrMgmt.Entity.Model;
+
+namespace Arvato.TestProject.UsrMgmt.Services
+{
+   public class AssetService : Contracts.IAssetService
+    {
+
+
+        public IEnumerable<Entity.Model.Asset> GetList(bool Enabled)
+        {
+            IAssetComponent component = new AssetComponent();
+            IEnumerable<Asset> result = null;
+
+            if (Enabled)
+            {
+                result = component.GetEnabledList();
+            }
+            else
+            {
+                result = component.GetList();
+            }
+            return result;
+
+         //   throw new NotImplementedException();
+        }
+
+        public bool InsertAsset(Entity.Model.Asset asset)
+        {
+            IAssetComponent component = new AssetComponent();
+            try
+            {
+                component.InsertAsset(asset);
+            }
+            catch (Exception ex)
+            {
+                
+                throw new FaultException(ex.Message);
+            }
+            return true;
+           // throw new NotImplementedException();
+        }
+
+        public bool UpdateAsset(Entity.Model.Asset entity)
+        {
+            IAssetComponent component = new AssetComponent();
+            try
+            {
+                component.UpdateAsset(entity);
+            }
+            catch (Exception ex)
+            {
+                
+                throw new FaultException(ex.Message);
+            }
+            return true;
+           
+           // throw new NotImplementedException();
+        }
+    }
+}
